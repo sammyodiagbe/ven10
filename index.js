829f8c81a9a456e8b1a7b35771799275aa9b5ecd
Odiagbe Samson <odiagbesamsonosaro@gmail.com>
2020-01-26 17:29:50 +0100

    fixing more bugs

--- index.js
+++ index.js
@@ -10,19 +10,30 @@ const PORT = process.env.PORT || 8000;
 const productRoutes = require("./routes/product_routes");
 
 // configuring cors from cross origin
-var whitelist = ["https://ven10-assesment.herokuapp.com", ""];
-var corsOptions = {
-    origin: function(origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    }
+const corsOptions = {
+    origin: "https://ven10-assesment.herokuapp.com" // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", ["https://ven10-assesment.herokuapp.com"]);
+
+    // Request methods you wish to allow
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+
+    // Request headers you wish to allow
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.json());
 app.use("/api/v1", productRoutes);
